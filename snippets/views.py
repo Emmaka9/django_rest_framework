7b1565d40fc71c54c6a5b6cac0e1f8918b547283
Emmaka9 <ikemmaka@ualr.edu>
2024-01-29 00:41:12 -0600

    last refactor using generics.ListCreateAPIView and generics.RetrieveUpdateDestroyAPIView

--- snippets/views.py
+++ snippets/views.py
@@ -1,28 +1,20 @@
+'''
+Refactor!!
+REST framework provides a set of already mixed-in generic views that can be used to trim down our views.py module even more.
+'''
+
 from snippets.models import Snippet
 from snippets.serializers import SnippetSerializer
-from rest_framework import mixins
 from rest_framework import generics
 
-class SnippetList(mixins.ListModelMixin, mixins.CreateModelMixin, generics.GenericAPIView):
+class SnippetList(generics.ListCreateAPIView):
+    
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
 
-    def get(self, request, *args, **kwargs):
-        return self.list(request, *args, **kwargs)
-    
-    def post(self, request, *args, **kwargs):
-        return self.create(request=request, *args, **kwargs)
-    
+class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
 
-class SnippetDetail(mixins.RetrieveModelMixin, mixins.UpdateModelMixin, mixins.DestroyModelMixin, generics.GenericAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
 
-    def retrieve(self, request, *args, **kwargs):
-        return self.retrieve(request, *args, **kwargs)
     
-    def update(self, request, *args, **kwargs):
-        return self.update(request, *args, **kwargs)
-    
-    def destroy(self, request, *args, **kwargs):
-        return self.destroy(request, *args, **kwargs)
