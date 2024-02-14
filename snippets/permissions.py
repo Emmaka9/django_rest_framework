5af83ce2563cbbf156dfa137bb8515270067e932
Imraul Emmaka <emmaka993@gmail.com>
2024-02-13 20:02:54 -0600

    api_root function for root view and SnippetHighlight class view added

--- snippets/permissions.py
+++ snippets/permissions.py
@@ -5,5 +5,17 @@ class IsOwnerOrReadOnly(permissions.BasePermission):
     Custom permission to only allow owners of an object to edit it.
     '''
 
-    
+    def has_object_permission(self, request, view, obj):
+        # Read permissions are allowed to any request
+        # so we'll always allow GET, HEAD, or OPTIONS requests.
 
+        if request.method in permissions.SAFE_METHODS:
+            # permissions.SAFE_METHODS is a tuple containing HTTP methods
+            # that are considered safe because they do not modify res
+            # typically GET, HEAD, OPTIONS
+            return True
+        
+        # Write permissions are only allowed to the owner of the snippet.
+        return obj.owner == request.user
+    
+    
--- snippets/views.py
+++ snippets/views.py
@@ -4,9 +4,20 @@ REST framework provides a set of already mixed-in generic views that can be used
 '''
 
 from snippets.models import Snippet
+from snippets.permissions import IsOwnerOrReadOnly
 from snippets.serializers import SnippetSerializer, UserSerializer
-from rest_framework import generics, permissions
+from rest_framework import generics, permissions, renderers
 from django.contrib.auth.models import User
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.reverse import reverse
+
+@api_view(['GET'])
+def api_root(request, format=None):
+    return Response({
+        'users': reverse('user-list', request=request, format=format),
+        'snippets': reverse('snippet-list', request=request, format=format)
+    })
 
 class SnippetList(generics.ListCreateAPIView):
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
@@ -18,10 +29,17 @@ class SnippetList(generics.ListCreateAPIView):
 
 class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
 
-    
-    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
+
+class SnippetHighlight(generics.GenericAPIView):
+    queryset = Snippet.objects.all()
+    renderer_classes = [renderers.StaticHTMLRenderer]
+
+    def get(self, request, *args, **kwargs):
+        snippet = self.get_object()
+        return Response(snippet.highlighted)
 
 '''
 Add read-only views for the user representations. Use ListAPIView and RetrieveAPIView generic class.
